0f5fc01e4668f689839a42af58e2581bfe5eec35
dian pratama <gilangr271101@gmail.com>
2022-04-24 14:55:53 +0800

    Initial commit

---
+++ app/src/main/java/com/F55119134/fragmentlayoutexmodul3/FragmentHome.java
@@ -0,0 +1,24 @@
+package com.F55119134.fragmentlayoutexmodul3;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+
+public class FragmentHome extends Fragment {
+    View view;
+    public FragmentHome() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.home_fragment, container, false);
+        return view;
+    }
+}
